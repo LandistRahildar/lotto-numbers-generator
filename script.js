81ae6c4084fa39656d2073c738c3f7fa2af8dafd
Mateusz Leszczyński <bxrq@wp.pl>
2023-05-18 09:51:50 +0200

    javascript code refactoring ('onclick' insead of 'addEventListener' for shorter code) and add id for button instead of class name

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 {
-    const lottoNumbersGenerateButton = document.querySelector(".js-random-lotto-numbers-button");
-    lottoNumbersGenerateButton.addEventListener("click", () => {
+    document.getElementById("randomLottoNumbersButton").onclick = function () {
         const numberFirst = Math.floor(Math.random() * 49 + 1);
         const numberSecond = Math.floor(Math.random() * 49 + 1);
         const numberThird = Math.floor(Math.random() * 49 + 1);
@@ -8,7 +7,7 @@
         const numberFifth = Math.floor(Math.random() * 49 + 1);
         const numberSixth = Math.floor(Math.random() * 49 + 1);
         document.getElementById("randomLottoNumbers").innerHTML = `Your random lotto numbers are ${numberFirst}, ${numberSecond}, ${numberThird}, ${numberFourth}, ${numberFifth}, ${numberSixth}.`;
-    });
+    }
 }
 
 //TO IMPROVE: lotto numbers can't repeat itself, but at this moment (10 V 23) I don't know how to do that condition(?) (I think I need use 'if' and 'else' for this); in the future I will update this little project
